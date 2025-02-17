da2911433174a499aaf416fd7ca39ab550586db8
Timi-Holberton <timi.holberton@gmail.com>
2025-02-17 13:33:27 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of char: %lu bytes\n", sizeof(char));
-printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of long int: %lu bytes\n", sizeof(long int));
+printf("Size of char: %ld bytes\n", sizeof(char));
+printf("Size of int: %ld bytes\n", sizeof(int));
+printf("Size of long int: %ld bytes\n", sizeof(long int));
 printf("Size of long long int : %ld bytes\n", sizeof(unsigned long int));
-printf("Size of float: %lu bytes\n", sizeof(float));
+printf("Size of float: %ld bytes\n", sizeof(float));
 return (0);
 }
