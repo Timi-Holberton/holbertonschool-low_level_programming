5925f32ce504252d4fc046d309d84871055b2a61
Timi-Holberton <timi.holberton@gmail.com>
2025-02-17 12:54:25 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * fonction taille de chaque type
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of long int: %lu bytes\n", sizeof(long int));
+printf("Size of long long int : %lu bytes\n", sizeof(unsigned long int));
+printf("Size of float: %lu bytes\n", sizeof(float));
+return (0);
+}
