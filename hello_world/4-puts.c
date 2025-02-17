2c9ffb29e36b05d587e00d29e90be6e4096829ea
Timi-Holberton <timi.holberton@gmail.com>
2025-02-17 11:13:24 +0000

    4.1

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,10 @@
 /* Puts afficher texte*/
 
 
-int main(void)
-{
-  puts('"Programming is like building a multilingual puzzle');
-}
+int main()
+{  
+  char *phrase1 = "\"Programming is like building a multilingual puzzle";
+puts(phrase1);
 
 return 0;
+}
