867566f2b60e3d25f9f8819251a77369a5a0d109
Timi-Holberton <timi.holberton@gmail.com>
2025-02-18 13:40:15 +0000

    4.1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * enlever un element d'une boucle
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alfaB;
+
+	for (alfaB = 'a'; alfaB <= 'z'; alfaB++)
+	{
+		if (alfaB != 'e' && alfaB != 'q')
+		{
+			putchar(alfaB);
+		}
+	}
+	putchar('\n');
+	
+	return(0);
+}
