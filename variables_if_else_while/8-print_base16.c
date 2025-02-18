1bc65fa41af1777d5253cc902b1bd9b0611275ef
Timi-Holberton <timi.holberton@gmail.com>
2025-02-18 15:03:13 +0000

    8.1

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hexa;
+
+	for (hexa = 0; hexa <= 16; hexa++)
+	{
+		putchar(hexa);
+	}
+	putchar('\n');
+
+	return (0);
+}
