007ba4a2a42dc0776c807463e7311fc6d122c36f
Timi-Holberton <timi.holberton@gmail.com>
2025-02-18 14:21:23 +0000

    6.1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+
+	return (0);
+}
