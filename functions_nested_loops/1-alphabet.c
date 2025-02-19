6fd12098d899c631e4321fa35f87282a9c3ebb2e
Timi-Holberton <timi.holberton@gmail.com>
2025-02-19 14:23:05 +0000

    1.3 commentaire

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * print_alphabet - Entry point
+ * utilisation prtotype void
  * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
