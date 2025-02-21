de59dcaaf2d23010fbb7f429218507325d098ba7
Timi-Holberton <timi.holberton@gmail.com>
2025-02-21 15:12:40 +0000

    9-10-11

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @nbre1 : nombre 1
+ * @nbre2 : nombre 2
+ * Return: Always 0 (Success)
+ */
+
+int add(int nbre1, int nbre2)
+{
+	return (nbre1 + nbre2);
+}
