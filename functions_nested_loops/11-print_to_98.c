2bfe02f35f226e611a330aee866f328281801170
Timi-Holberton <timi.holberton@gmail.com>
2025-02-22 22:33:50 +0000

    11.2 virgule ok

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,22 +12,24 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
+		printf("%d", n);
 		do {
-			printf("%d, ", n);
+			printf(", %d", n);
 			n++;
 		} while (n <= 98);
 	}
 	else if (n >= 98)
 	{
+		printf("%d", n);
 		do {
-			printf("%d, ", n);
+			printf(", %d", n);
 			n--;
 		} while (n >= 98);
 
 	}
 	else
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
