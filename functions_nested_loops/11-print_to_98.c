a4a4cf27218a7e2914d7cbda81d87d77d0beb11c
Timi-Holberton <timi.holberton@gmail.com>
2025-02-22 23:15:53 +0000

    11.4 enlever do

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,22 +10,16 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		printf("%d", n);
-		do {
-			printf(", %d", n);
-			n++;
-		} while (n <= 98);
+		while (n < 98)
+			printf("%d, ", n++);
 	}
-	else if (n > 98)
+
+	else
 	{
-		printf("%d", n);
-		do {
-			printf(", %d", n);
-			n--;
-		} while (n >= 98);
-	} else
-		printf("%d", n);
-	printf("\n");
+		while (n > 98)
+			printf("%d, ", n--);
+	}
+	printf("%d\n", n);
 }
