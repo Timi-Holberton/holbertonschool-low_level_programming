73ed83950d64e369f4e0354f705d5a50a9c45a1c
Timi-Holberton <timi.holberton@gmail.com>
2025-02-22 16:59:00 +0000

    9.1 pb virgule

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,11 +12,28 @@ void times_table(void)
 	int multiple1, multiple2, table;
 
 	for (multiple1 = 0; multiple1 <= 9; multiple1++)
+	{
+		_putchar('\n');
+
 		for (multiple2 = 0; multiple2 <= 9; multiple2++)
 		{
-			table = multiple1*multiple2;
-			_putchar(table);
-			_putchar(32);
-			_putchar('\n');
+			table = multiple1 * multiple2;
+			if (table >= 10)
+			{
+				_putchar('0' + (table / 10));
+				_putchar('0' + (table % 10));
+				_putchar(44);
+				_putchar(32);
+
+	      			}
+			else
+			{
+				_putchar('0' + (table % 10));
+                                _putchar(44);
+                                _putchar(32);
+				_putchar(32);
+			}
+
 		}
+	}
 }
