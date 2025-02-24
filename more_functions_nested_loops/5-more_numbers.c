041ef368cdeb79139ab79e5dc547a6d91890275f
Timi-Holberton <timi.holberton@gmail.com>
2025-02-24 13:38:36 +0000

    5.1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		int j;
+		char tableau[] = "01234567891011121314";
+		for (j = 0; j <= 20; j++)
+		{
+			_putchar(tableau[j]);
+
+		}
+		_putchar('\n');
+	}
+}
