2f6f0fd512bec9f3b9c39986f36377f973a44f09
Timi-Holberton <timi.holberton@gmail.com>
2025-02-24 15:31:57 +0000

    6.3 saut de ligne

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -12,15 +12,16 @@ void print_line(int n)
 	int b;
 
 	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar(95);
+		}
 		_putchar('\n');
 
-	else
 
-		for (b = 1; b <= n; b++)
-		{
-		_putchar(95);
-
-		}
-
-	_putchar('\n');
+	}
 }
