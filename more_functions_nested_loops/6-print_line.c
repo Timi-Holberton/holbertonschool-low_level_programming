b9752626743df77ac911850f5e9386f354668fbb
Timi-Holberton <timi.holberton@gmail.com>
2025-02-24 15:01:26 +0000

    6.1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n : var
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+	int b;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	if (n > 0)
+
+		for (b = 1; b <= n; b++)
+		{
+		_putchar(95);
+
+		}
+	_putchar('\n');
+
+}
