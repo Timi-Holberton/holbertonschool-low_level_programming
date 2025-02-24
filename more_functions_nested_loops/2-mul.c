3348c2a9f51cabd7f8cfde60292034f8c4e7c8ee
Timi-Holberton <timi.holberton@gmail.com>
2025-02-24 10:47:48 +0000

    2.1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a : var
+ * @b : var
+ * Return: Always 0 (Success)
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -12,3 +12,4 @@ void print_to_98(int n);
 void times_table(void);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
