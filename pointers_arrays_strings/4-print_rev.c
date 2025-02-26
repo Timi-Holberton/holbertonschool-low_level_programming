7c97f02c2fb59071c7d1fe40cfae5e6e464ea2cf
Timi-Holberton <timi.holberton@gmail.com>
2025-02-26 13:03:47 +0000

    4.1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s : var
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	putchar('\n');
+}
