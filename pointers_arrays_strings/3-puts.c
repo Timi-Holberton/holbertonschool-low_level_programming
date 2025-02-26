e9603a0b40d8a5076af056748ea2767973859a2d
Timi-Holberton <timi.holberton@gmail.com>
2025-02-26 12:22:07 +0000

    3.1

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str : var
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i[str] != '\0')
+	{
+		_putchar(i[str]);
+		i++;
+	}
+		_putchar('\n');
+}
