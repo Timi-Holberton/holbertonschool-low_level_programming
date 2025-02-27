95b1c21721eea60df756560475281f69205cd3b9
Timi-Holberton <timi.holberton@gmail.com>
2025-02-27 12:56:21 +0000

    6.1

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str : var
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (i[str] != '\0')
+	{
+		_putchar(i[str]);
+		i += 2;
+	}
+	_putchar('\n');
+}
