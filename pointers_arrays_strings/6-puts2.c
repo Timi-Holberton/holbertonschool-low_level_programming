4e294ba1395ed63a18fa2b324b315212988217a1
Timi-Holberton <timi.holberton@gmail.com>
2025-02-27 13:29:42 +0000

    6.3

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (i[str] != '\0')
-	{
-		_putchar(i[str]);
-		i += 2;
-	}
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
 	_putchar('\n');
 }
