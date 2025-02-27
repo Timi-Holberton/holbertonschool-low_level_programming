caf605ec0796b187be43ef2b55b13fa4258462f0
Timi-Holberton <timi.holberton@gmail.com>
2025-02-27 21:00:09 +0000

    9.1

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy  - Entry point
+ * @dest : destination
+ * @src : source
+ * Return: Always 0 (Success)
+ */
+
+char *_strcpy(char *dest, char *src) /* destination, source */
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+       		printf("%s", src);
+	}
+
+	return (dest);
+}
