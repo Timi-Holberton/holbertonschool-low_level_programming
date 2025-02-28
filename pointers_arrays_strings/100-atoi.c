620501146f6d9a2adf1a186264de6b298166000e
Timi-Holberton <timi.holberton@gmail.com>
2025-02-28 12:15:48 +0000

    10.3

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,31 +10,11 @@
 int _atoi(char *s)
 {
 	int result = 0;
-	int moins = 1;
 
-	if  ((*s <= '0') && (*s >= '9'))
+	while ((*s <= '0') && (*s >= '9'))
 	{
 		result = (result * 10 + (*s - '0'));
+		s++;
 	}
-	return (result * moins);
+	return (result);
 }
-
-/**
-Int stringToInt(const char s[])
-{
-	int res = 0;
-	int i = 0;
-
-	while (i[s] != '\0')
-	{
-		i++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		res *= 10;
-		res += s[i] - '0';
-	}
-	return res;
-}
-*/
