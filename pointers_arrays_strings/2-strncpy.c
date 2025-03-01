aaa2ead6263af7acde66e1e4c74ec8fed88774ec
Timi-Holberton <timi.holberton@gmail.com>
2025-03-01 21:57:43 +0100

    main.h

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest : destination
+ * @src: source
+ * @n : nombre et emplacement
+ * Return: Always 0 (Success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	/* si src moins long que n, ajouter des \0 à dest*/
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
