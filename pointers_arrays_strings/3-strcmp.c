fe4f552cf98a5c21f4cf9748a4996a0524326641
Timi-Holberton <timi.holberton@gmail.com>
2025-03-02 07:37:41 +0100

    3.5

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,23 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	for (i = 0; s1[i] != '\0'; i++)
-
-	for (j = 0; s2[j] != '\0'; j++)
-
-	if (s1[i] == s2[j])
-	{
-		return (0);/* code */
+	int i = 0;
+/* compare un a un chaque caractère */
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{/* si 1 caractère diffère on soustrait*/
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);/* code */
+		}
+		i++; /* penser a incrémenter dans while*/
 	}
-	else if (s1[i] > s2[j])
-	{
-		return (i > 0);
-	}
-	else
-	{
-		return (i < 0);
-	}
-	return (i);
+	return (s1[i] - s2[i]);
 }
