9dc8e5e9fa2d668dd69becdc47fd6dbb2b7332e1
Timi-Holberton <timi.holberton@gmail.com>
2025-03-02 22:41:06 +0100

    100.4

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,12 +9,31 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
+	int i;
+	int negatif = 1;
+	int nombre = 0;
 
-	while ((*s <= '0') && (*s >= '9'))
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		result = (result * 10 + (*s - '0'));
-		s++;
+		if (s[i] == '-')
+		{
+			negatif *= -1; /* On inverse le signe */
+		}
+		else if (s[i] == '+')
+		{
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			/* début du nombre, on sort pour lire les chiffres */
+			break;
+		}
 	}
-	return (result);
+
+	/* pour lire les chiffres du nombre */
+	for (; s[i] >= '0' && s[i] <= '9'; i++)
+	{
+		nombre = nombre * 10 + (s[i] - '0');
+	}
+
+	return (nombre * negatif);
 }
