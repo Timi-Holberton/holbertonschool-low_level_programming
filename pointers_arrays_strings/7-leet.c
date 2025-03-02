a323379baf41d109167308f720673cfbbd3faeab
Timi-Holberton <timi.holberton@gmail.com>
2025-03-02 16:01:02 +0100

    7.1

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -12,8 +12,11 @@ char *leet(char *tab)
     int i;
 
     for(i = 0; tab[i] = '\0; i++')
-        if ((tab[i] >= 65) && ((tab[i] <= 90) || ((tab[i] >= 97) && ((tab[i] <= 122)
+    {
+        if ((tab[i] >= 65) && (tab[i] <= 90)) || ((tab[i] >= 97) && (tab[i] <= 122))
         {
 
         }
     }
+    return (tab);
+}
