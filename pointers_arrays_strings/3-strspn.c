f0e8d4d3e896967a9a16b9f02f2f9d398ef89e9a
Timi-Holberton <timi.holberton@gmail.com>
2025-03-03 17:10:57 +0100

    3.1

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn : calcule la longueur du préfixe initial de la chaîne 's'
+ * qui ne contient que des caractères présents dans la chaîne 'accept'
+ * @s : chaine expérimental que l'on compare à accept
+ * @accept : chaine témoin
+ * Return: retourne le nombre de caractères consécutifs au début de 's'
+ * qui appartiennent à 'accept'
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int cara;	/* Variable pour parcourir la chaîne 's' */
+	int ctere;	/* Variable pour parcourir la chaîne 'accept' */
+
+	/* Parcours de chaque caractère de 's'
+	*jusqu'à rencontrer le caractère nul '\0'
+	*/
+	for (cara = 0; s[cara] != '\0'; cara++)
+	{
+/* Parcours de 'accept' pour vérifier si
+ * le caractère courant de 's' est présent
+*/
+		for (ctere = 0; accept[ctere] != s[cara]; ctere++)
+		{
+/*
+ * Si on atteint la fin de 'accept' ('\0') sans trouver le caractère de 's',
+ * cela signifie que le caractère courant de 's' n'est pas accepté.
+ * On retourne alors le nombre de caractères valides comptés jusqu'ici (cara).
+ */
+			if (accept[ctere] == '\0')
+			{
+				return (cara);
+			}
+		}
+/*
+ * Si on sort de la boucle interne sans retourner,
+ * cela veut dire que le caractère de 's' a bien été trouvé dans 'accept'.
+ * On passe donc au caractère suivant de 's'.
+ */
+	}
+/*
+ * Si on a parcouru toute la chaîne 's' sans rencontrer de caractère non accepté,
+ * on retourne la longueur totale de 's'.
+ */
+	return (cara);
+}
+/** la chaine s recherche dans la chaine accept si elle as des caractères
+ * identique a accept et si ce n'est plus le cas elle affiche le nombre
+ * de caractère en commun
+*/
