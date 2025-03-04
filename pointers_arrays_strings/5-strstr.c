a5557530f33f9f34d92ae4b92fd568e3c1dbbab8
Timi-Holberton <timi.holberton@gmail.com>
2025-03-04 16:07:38 +0100

    5.2

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,20 +2,26 @@
 #include "main.h"
 
 /**
- *  - Entry point
- *
+ * _strstr - Entry point
+ * @haystack : chaine
+ * @needle : sous chaine
  * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j; /* Déclaration d'un compteur pour parcourir la chaîne */
-	 /* Initialisation de l'index à 0 */
+	int i, j;
+	/* Déclaration de variables pour parcourir 'haystack' et 'needle */
+
+	/* Parcours chaque caractère de la chaîne 'haystack' */
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] == haystack[i]; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			return (&haystack[i]);
+			if (haystack[i] != needle[j])
+			{
+				return (&needle[i]);
+			}
 		}
 	}
-	return (0);
+	return (0);	/* Retourne NULL si la sous-chaîne n'est pas trouvée */
 }
