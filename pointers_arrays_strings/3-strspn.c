5e3f77fb18ab58ad73b4e347f3a144e792ce18da
Timi-Holberton <timi.holberton@gmail.com>
2025-03-04 14:15:01 +0100

    3.3 commentaire

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,32 +1,31 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
- * _strspn - Calcule la longueur du préfixe de 's'
- * qui ne contient que des caractères présents dans 'accept'.
+/*
+ * _strspn - Retourne la longueur du préfixe de 's'
+ * avec seulement des caractères de 'accept'.
  * @s: chaîne à analyser.
- * @accept: chaîne contenant les caractères autorisés.
- * Return: nombre de caractères consécutifs de 's'
- * présents dans 'accept'.
+ * @accept: caractères autorisés.
+ * Return: nombre de caractères valides au début de 's'.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;  /* Compteur pour parcourir la chaîne 's' */
-	int j; /* Compteur pour parcourir la chaîne 'accept' */
+	unsigned int i, j;
 
+	/* Parcourt chaque caractère de 's' */
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		/* Vérifie si s[i] est dans 'accept' */
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			{
-				break;
-			}
+				break; /* Trouvé, on passe au suivant */
 		}
+
+		/* Si s[i] n'est pas trouvé dans 'accept', on arrête */
 		if (accept[j] == '\0')
-		{
 			break;
-		}
 	}
+
+	/* Retourne le nombre de caractères valides */
 	return (i);
 }
