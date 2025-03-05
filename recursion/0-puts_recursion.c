86beb2ce1d9305c4d7a5f4bd104076b68af86527
Timi-Holberton <timi.holberton@gmail.com>
2025-03-05 09:56:28 +0100

    0.1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,7 +2,17 @@
 #include "main.h"
 
 /**
- *  - Entry point
- *
+ * _puts_recursion - Entry point
+ * @s : string
  * Return: Always 0 (Success)
  */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
