b2f6b6d0bcdd98348ff1176c9de1fefc756b7e64
Timi-Holberton <timi.holberton@gmail.com>
2025-03-05 13:38:52 +0100

    main 0.2 1.1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s : string
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
