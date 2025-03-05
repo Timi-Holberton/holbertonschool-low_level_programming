33f99b7c97bdc493247c2cebec6539851c81160f
Timi-Holberton <timi.holberton@gmail.com>
2025-03-05 14:53:15 +0100

    2.1

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - Entry point
+ * @s : string
+ * Return: Always 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return ((1) + _strlen_recursion(s + 1));
+}
