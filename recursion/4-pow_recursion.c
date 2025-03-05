a9e823a25eea1e45206ca77247ec3222528ed0df
Timi-Holberton <timi.holberton@gmail.com>
2025-03-05 16:22:46 +0100

    4.1

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @x : noombre
+ * @y : puissance
+ * Return: Always 0 (Success)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return ((x) * _pow_recursion(x, y - 1));
+}
