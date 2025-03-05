64cb356b3aa1838d930254f1cbd633e58e10e751
Timi-Holberton <timi.holberton@gmail.com>
2025-03-05 09:19:51 +0100

    7(6).2

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,24 +2,21 @@
 #include "main.h"
 
 /**
- *  - Entry point
- *
+ * print_chessboard - Entry point
+ *@a : tableau pointe sur un autre tableau
  * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	char i, j;
-	char c;
-	char tab[8];
-	(*a)[8] = &tab;
-	c = (*a)[0];
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
+
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(c);
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
-
