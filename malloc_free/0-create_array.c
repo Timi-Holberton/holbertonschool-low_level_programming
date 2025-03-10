3554050150331b0e88e86a365c230c56c715d649
Timi-Holberton <timi.holberton@gmail.com>
2025-03-10 11:00:50 +0100

    0.1 main readme

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - Entry point
+ *@size : taille
+ *@c : caractère dans tableau
+ * Return: Always 0 (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *tab = malloc(size * sizeof(char));
+
+		if (tab == NULL)
+		{
+			printf("Échec de l'allocation\n");
+		return (NULL);
+		}
+
+		for(i = 0; i < size; i++)
+		{
+			tab[i] = c;
+		}
+		return (tab);
+		free(tab);
+}
