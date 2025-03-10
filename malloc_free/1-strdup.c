e911d87934ca594c0c7a60a02b2993416c073a09
Timi-Holberton <timi.holberton@gmail.com>
2025-03-10 14:50:52 +0100

    1.1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  - Entry point
+ *@str : tableau
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	char *dest = malloc((i+ 1) * sizeof(char));
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dest[i] = str[i];
+	}
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	return(dest);
+}
