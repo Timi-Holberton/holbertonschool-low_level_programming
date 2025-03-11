405ed6b1bae83ecfc9c076b932139b4f01634c64
Timi-Holberton <timi.holberton@gmail.com>
2025-03-11 16:31:18 +0100

    4.9

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -12,7 +12,12 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i >= 0; i--)
+	if (grid = NULL || height <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
 	}
