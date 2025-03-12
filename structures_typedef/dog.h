21a1c4d576feb2840169ae41492f4c8be6eca458
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 15:14:07 +0100

    0.3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ *@name : nom
+ *@age : age
+ *@owner : propriétaire
+ * Return: Always 0 (Success)
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
