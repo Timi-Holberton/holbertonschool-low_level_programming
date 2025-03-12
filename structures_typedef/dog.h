a20c7760e4685c33a1fadd0f2c1d4aa29ad6346d
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 23:38:50 +0100

    3.3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,22 +2,22 @@
 #define DOG_H
 
 /**
- * struct dog - Entry point
+ * struct dog_t - Entry point
  *@name : nom
  *@age : age
  *@owner : propriétaire
  * Return: Always 0 (Success)
  */
 
-typedef dog_t
+struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void init_dog(struct dog_t *d, char *name, float age, char *owner);
+void print_dog(struct dog_t *d);
 
 
 #endif /* DOG_H */
