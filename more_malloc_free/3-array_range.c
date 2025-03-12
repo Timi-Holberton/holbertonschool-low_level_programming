0383609b4ffa7eaa3631e6a564a72f5b623f958b
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 13:48:22 +0100

    3.1

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Entry point
+ *@min : minimum
+ *@max : maximum
+ * Return: Always 0 (Success)
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *tab;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	tab = malloc((max - min + 1) * sizeof(int));
+	if (!tab)
+	{
+		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		tab[i] = i;
+	}
+return (tab);
+}
