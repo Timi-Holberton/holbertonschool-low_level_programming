f42272fd10e69a5e3291df72075237057717c907
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 17:09:24 +0100

    1.1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ *@d : structure
+ *@name : nom
+ *@age : age
+ *@owner : propriétaire
+ * Return: Always 0 (Success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
