4df4e00ee8137caf3da273be24d88c93ee32c1b4
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 23:20:06 +0100

    2.2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -31,8 +31,7 @@ void print_dog(struct dog *d)
 		printf("nil");
 	}
 
-
-	printf("Name : %s\n", d->name);
+	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
