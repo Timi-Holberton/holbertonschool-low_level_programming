36d336a9a591797725005d3266f6f7796b542cf0
Timi-Holberton <timi.holberton@gmail.com>
2025-03-12 23:46:13 +0100

    3.5

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,20 +9,12 @@
  * Return: Always 0 (Success)
  */
 
-struct dog
+typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
