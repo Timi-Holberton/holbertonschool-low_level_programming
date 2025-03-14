a6bb348a5750c36482857170f26544a6b6cd1123
Timi-Holberton <timi.holberton@gmail.com>
2025-03-14 16:26:06 +0100

    2.11

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,33 +14,33 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL) /* Vérifie si le pointeur est NULL */
-		{
-			return;    /* Si oui, on quitte la fonction */
-		}
+	{
+		return;    /* Si oui, on quitte la fonction */
+	}
 
 	if (d->name == NULL)  /* Vérifie si le nom est NULL */
-		{
-			printf("Name: (nil)\n");
-		}
-	else
-		{
-			printf("Name: %s\n", d->name);
-		}
-	if (d->age == 0)  /* Vérifie si l'âge est 0 */
-		{
-			printf("Age: (nil)\n");
-		}
-	else
-		{
-			printf("Age: %f\n", d->age);
-		}
-	if (d->owner == NULL) /* Vérifie si le propriétaire est NULL */
-		{
-			printf("Owner: (nil)\n");
-		}
+	{
+		printf("Name: (nil)\n");
+	}
 	else
 	{
-			printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name);
+	}
+	if (d->age == 0)  /* Vérifie si l'âge est 0 */
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
+	if (d->owner == NULL) /* Vérifie si le propriétaire est NULL */
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
 	}
 }
 
