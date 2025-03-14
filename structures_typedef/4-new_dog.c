6094a101322ec703397f388dbf0b8aa397b62bde
Timi-Holberton <timi.holberton@gmail.com>
2025-03-14 16:55:17 +0100

    4.10

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t *d = malloc(sizeof(dog_t));
 
-	if (d == NULL || name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
 	for (long1 = 0; name[long1] != '\0'; long1++)
