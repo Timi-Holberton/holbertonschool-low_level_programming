aea10d09014314e578fdd51e289cf1e73a951fd6
Timi-Holberton <timi.holberton@gmail.com>
2025-03-17 10:42:04 +0100

    0.2

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,12 +2,22 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f : pointeur fonctions
- * Return: Nothing.
+ * print_name - Affiche un nom en utilisant une fonction fournie.
+ * @name: Nom de la personne à afficher.
+ * @f: Pointeur vers une fonction prenant un char *.
+ *
+ * Cette fonction appelle la fonction pointée par @f en lui
+ * passant le nom @name. Cela permet d'afficher le nom de
+ * différentes manières selon la fonction fournie.
+ *
+ * Return: Rien.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	/* Vérifie si les paramètres sont valides */
+	if (name != NULL && f != NULL)
+	{
+		/* Appelle la fonction pointée par f avec name */
+		(*f)(name);
+	}
 }
