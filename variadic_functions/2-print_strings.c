ac174c24fd38d9318b8199a6fa8ece5a47856a69
Timi-Holberton <timi.holberton@gmail.com>
2025-03-20 16:52:57 +0100

    2.1 programme print strings with function variadic

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - imprime un texte avec séparateur
+ * @separator: string pour séparer nombre
+ * @n: le nombre d'entier dans la fonctions
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *texte;
+	va_list argument;
+
+	va_start(argument, n);
+
+	for (i = 0; i < n; i++)
+	{
+		texte = va_arg(argument, char*);
+		printf("%s", texte);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(argument);
+}
