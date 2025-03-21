0c9134a38ed5ebc3d9b229a4f4aa6bedb819f203
Timi-Holberton <timi.holberton@gmail.com>
2025-03-21 18:52:12 +0100

    3.7

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -10,8 +10,8 @@
  */
 typedef struct format1
 {
-    char symbol;
-    void (*print_function)(va_list args);
+	char symbol;
+	void (*print_function)(va_list args);
 } format1;
 
 /* Prototypes des fonctions */
@@ -22,22 +22,3 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
-
-/**
- * Lexique :
- * - typedef : Définit un alias pour un type.
- * - struct : Structure qui regroupe plusieurs variables.
- * - va_list : Type pour stocker des arguments variables.
- * - va_start : Initialise la liste des arguments.
- * - va_arg : Extrait un argument de la liste.
- * - va_end : Termine l'utilisation de va_list.
- */
-
-/**
- * Rappels des notions en C :
- * - Les structures permettent d'associer plusieurs types sous un seul nom.
- * - Un pointeur de fonction stocke l'adresse d'une fonction et permet
- *   de l'exécuter dynamiquement.
- * - Les prototypes de fonction définissent les signatures des fonctions
- *   avant leur implémentation.
- */
