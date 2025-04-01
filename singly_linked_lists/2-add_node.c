3a459d36932f3cb2c194809970a328ab0b10d2c6
Timi-Holberton <timi.holberton@gmail.com>
2025-04-01 11:00:56 +0200

    2.2 ajout fonction strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,25 +1,50 @@
 #include "lists.h"
 
+/**
+ * add_node - insère un noeud
+ *@head: Noeud de tête
+ *@str: string
+ * Return: new_node
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	if (!new_node)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (!str) {
+	{
 		free(new_node);
 		return (NULL);
 	}
+
+	if (!str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
 
 	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
+	new_node->len = strlen(new_node->str);
+	
 	new_node->next = *head;
 	*head = new_node;
-
 
 	return (new_node);
 }
