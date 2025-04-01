3970e0bf478cb024ab6d8434b6134f2da7b39c9e
Timi-Holberton <timi.holberton@gmail.com>
2025-04-01 16:21:34 +0200

    3.2 ajout vérification si head et str == NULL

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -22,16 +22,17 @@ int _strlen(char *s)
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *end = *head;
+	list_t *new_node;
+	list_t *end;
 
-	if (!new_node)
-	{
+	if (head == NULL || str == NULL)
 		return (NULL);
-	}
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
 	new_node->str = strdup(str);
-
 	if (!new_node->str)
 	{
 		free(new_node);
@@ -44,14 +45,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (NULL);
+		return (new_node);
 	}
-
+	end = *head;
 	while (end->next != NULL)
 	{
 		end = end->next;
 	}
-
 	end->next = new_node;
 
 	return (new_node);
