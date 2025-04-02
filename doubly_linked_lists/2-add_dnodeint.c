030d2dae4338f4a2e0258f2f8543dde81024672e
Timi-Holberton <timi.holberton@gmail.com>
2025-04-02 11:04:59 +0200

    2.2 betty correction

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
@@ -19,9 +20,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->prev = NULL;
 
-	if (*head != NULL) {
+	if (*head != NULL)
 		(*head)->prev = new_node;
-	}
+
 	*head = new_node;
 
 	return (new_node);
