5bc2c6fd6ec3b06c7ae4995d1b8f69857c4fb497
Timi-Holberton <timi.holberton@gmail.com>
2025-04-02 10:05:08 +0200

    1.2 description fonction betty

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_dlistint - check the code
+ * dlistint_len - affiche le nombre d'élément
  *@h: pointeur vers le noeud suivant
  * Return: stock, nombre de charactère
  */
 
- size_t dlistint_len(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t stock = 0;
 
