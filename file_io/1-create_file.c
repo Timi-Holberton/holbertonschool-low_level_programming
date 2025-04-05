f32b779e871fe82e3ca6ee8f2c1fd6b22ac8d114
Timi-Holberton <timi.holberton@gmail.com>
2025-04-05 12:55:01 +0200

    1.3 corrections suppression erreur

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,7 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
