dcdb0f73e727af16655e5551cc0c5c46d48fca02
Timi-Holberton <timi.holberton@gmail.com>
2025-04-05 12:49:24 +0200

    1.2  échange oerlissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,32 +16,22 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
-
-	else
+	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
 		{
-			stock_write = write(fd, text_content, i);
 			i++;
 		}
+		stock_write = write(fd, text_content, i);
+
 		if (stock_write == -1 || stock_write != i)
 		{
 			close(fd);
 			return (-1);
 		}
-	}
-
-	if (fd == -1)
-	{
-		close(fd);
-		return (-1);
 	}
 
 	close(fd);
--- file_io/main.h
+++ file_io/main.h
@@ -14,6 +14,7 @@
 #include <stdarg.h>
 #include <time.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
