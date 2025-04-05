346f7c11c1ddc0c97bb137c4851c9b9141bf1ecb
Timi-Holberton <timi.holberton@gmail.com>
2025-04-05 11:25:40 +0200

    1.1 Crée un fichier et écrit dedans

---
+++ file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * create_file - Crée un fichier et écrit dedans
+ * @filename: Nom du fichier à créer
+ * @text_content: Texte à écrire dans le fichier (NULL = fichier vide)
+ * Return: 1 en cas de succès, -1 en cas d'erreur
+ * (échec de création ou d'écriture).
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int stock_write;
+	int i = 0;
+
+	if (filename == NULL)
+	return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	else
+	{
+		while (text_content[i] != '\0')
+		{
+			stock_write = write(fd, text_content, i);
+			i++;
+		}
+		if (stock_write == -1 || stock_write != i)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
